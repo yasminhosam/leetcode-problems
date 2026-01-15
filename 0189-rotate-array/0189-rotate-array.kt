0a4aef51ea8c64c62386ec9f0499e6fb614c7535
Yasmin <142786474+yasminhosam@users.noreply.github.com>
2026-01-15 16:56:16 +0200

    Time: 16 ms (20.57%), Space: 59.1 MB (96.88%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.kt
+++ 0189-rotate-array/0189-rotate-array.kt
@@ -1,14 +1,9 @@
 class Solution {
     fun rotate(nums: IntArray, k: Int): Unit {
-        val n=nums.size
-        val shifts=k%n
-       val rotated = IntArray(n)
-        for(i in 0..n-1){
-            rotated[(i+shifts) %n]=nums[i]
-        }
-        for(i in 0..n-1){
-            nums[i]=rotated[i]
-        }
+        var k=(k%nums.size)
+        nums.reverse()
+        nums.reverse(0,k)
+        nums.reverse(k,nums.size)
         
     }
 }
